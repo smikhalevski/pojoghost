ed64f4f2017ff8d502b8d5d1f19d3ee6a273f7b4
Savva Mikhalevski <smikhalevski@gmail.com>
2014-02-24 21:44:31 +0400

    Removed excessive ConverterRegistryAware interface

--- src/main/java/org/ehony/pojoghost/core/TypeConverter.java
+++ src/main/java/org/ehony/pojoghost/core/TypeConverter.java
@@ -6,7 +6,11 @@
  */
 package org.ehony.pojoghost.core;
 
-public interface TypeConverter<A, B> extends Converter<A, B>, ConverterRegistryAware {
+public interface TypeConverter<A, B> extends Converter<A, B> {
 
     void map(Field<A, ?> from, Field<B, ?> to, Converter converter);
+    
+    ConverterRegistry getRegistry();
+    
+    void setRegistry(ConverterRegistry registry);
 }
